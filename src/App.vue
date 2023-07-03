0c19561b23894753400660ee7955e2bd7af36041
GLCVLL <g.ioele96@outlook.it>
2023-07-03 17:05:51 +0200

    added circles

--- src/App.vue
+++ src/App.vue
@@ -4,15 +4,28 @@ export default {
 };
 </script>
 <template>
+  <header></header>
   <main>
-    <div class="container card-container">
-      <div class="row">
+    <div class="container">
+      <!-- cerchi -->
+      <div class="circles d-flex align-items-center py-3">
+        <div class="big-circle">
+        </div>
+        <div class="small-circles d-flex px-2">
+          <span class="bg-danger"></span>
+          <span class="bg-warning"></span>
+          <span class="bg-success"></span>
+        </div>
+      </div>
+      <!-- pokemon list -->
+      <div class="row card-container">
         <div class="col">
           <div class="card"></div>
         </div>
       </div>
     </div>
   </main>
+  <footer></footer>
 </template>
 <style lang="scss">
 @use './assets/scss//style.scss';
