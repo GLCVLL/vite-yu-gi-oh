d28bd5afb088766390155ce390ffc870f4b5e2bc
Gioele Iacovella <g.ioele96@outlook.it>
2023-07-03 16:30:09 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+
+};
+</script>
+<template>
+  <main>
+    <div class="container card-container">
+      <div class="row">
+        <div class="col">
+          <div class="card"></div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+<style lang="scss">
+@use './assets/scss//style.scss';
+</style>
